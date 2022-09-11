4be95d5a6b553771633ff21250e2bcfcd0d872b6
aksource <akira.mew@gmail.com>
2022-09-11 21:23:41 +0900

    thisの使い方追加

--- src/main/java/ak/mcmod/ak_mixins/mixin/MixinServerGamePacketListenerImpl.java
+++ src/main/java/ak/mcmod/ak_mixins/mixin/MixinServerGamePacketListenerImpl.java
@@ -3,6 +3,7 @@ package ak.mcmod.ak_mixins.mixin;
 import ak.mcmod.ak_mixins.AkMixins;
 import ak.mcmod.ak_mixins.ConfigUtils;
 import javax.annotation.Nonnull;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,16 +14,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * ServerGamePacketListenerImplに対する操作を行うクラス
- * このクラス自体がServerGamePacketListenerImplになるので、thisからprivate変数等取れる
- * Created by A.K. on 2022/08/24.
+ * このクラス自体がServerGamePacketListenerImplになるが、メンバー変数を呼び出す場合はShadowアノテーションを付けた変数を宣言する Created by A.K.
+ * on 2022/08/24.
  */
 @Mixin(ServerGamePacketListenerImpl.class)
 public class MixinServerGamePacketListenerImpl {
 
-  @Shadow private int tickCount;
+  @Shadow
+  private int tickCount;
+
+  @Shadow
+  public ServerPlayer getPlayer() {
+    return null;
+  }
 
   /**
    * handleMovePlayerのローカル変数f2のLOAD時の処理に割り込む
+   *
    * @param f2 プレイヤーの移動距離の制限判定時に利用されるfloat型変数
    * @return f2のLOADの際に代わりに返却する値
    */
@@ -30,11 +38,15 @@ public class MixinServerGamePacketListenerImpl {
   private float hookHandleMovePlayerForMovingLimit(float f2) {
     // thisの使用例 tickCountはprivate変数
     AkMixins.LOGGER.info(this.tickCount);
-    return ConfigUtils.COMMON.modifyPlayerServerMovingLimit ? (float) ConfigUtils.COMMON.playerServerMovingLimit : f2;
+    // thisの使用例 getPlayerメソッドはServerPlayerConnectionインターフェースのメソッド
+    this.getPlayer();
+    return ConfigUtils.COMMON.modifyPlayerServerMovingLimit
+        ? (float) ConfigUtils.COMMON.playerServerMovingLimit : f2;
   }
 
   /**
    * isSingleplayerOwnerメソッドの最初に処理を割り込ませるメソッド
+   *
    * @param callbackInfo CallbackInfo
    */
   @Inject(method = "isSingleplayerOwner", at = @At(value = "HEAD"), cancellable = true)
